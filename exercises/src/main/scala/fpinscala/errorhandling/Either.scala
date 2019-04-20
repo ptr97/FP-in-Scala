6f3ea63d9ee7d1fcb3bea70ee040caf193c66a35
Piotr <ptr97@users.noreply.github.com>
2019-04-20 16:11:25 +0200

    Finish Either exercises

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,21 +4,48 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = ???
+ def map[B](f: A => B): Either[E, B] = {
+   this match {
+     case Left(er) => Left(er)
+     case Right(v) => Right(f(v))
+   }
+ }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = {
+   this match {
+     case Left(er) => Left(er)
+     case Right(v) => f(v)
+   }
+ }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+ def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = {
+   this match {
+     case Right(v) => Right(v)
+     case Left(_) => b
+   }
+ }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+ def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+   for {
+     a  <- this
+     b1 <- b
+   } yield f(a, b1)
+ }
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = {
+    es match {
+      case Nil => Right(Nil)
+      case h :: t => f(h).map2(traverse(t)(f))(_ :: _)
+    }
+  }
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = {
+    traverse(es)(x => x)
+  }
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
     if (xs.isEmpty) 
